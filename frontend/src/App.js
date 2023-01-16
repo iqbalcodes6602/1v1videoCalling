de3e6ac818f53edfbc89a1845e99c41f0d707b93
noobcoder6602 <mohd.abd.6602@gmail.com>
2023-01-16 16:01:03 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,150 @@
+import React, { useEffect, useRef, useState } from "react"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import Peer from "simple-peer"
+import io from "socket.io-client"
+import "./App.css"
+
+
+const socket = io.connect('http://localhost:5000')
+function App() {
+	const [me, setMe] = useState("")
+	const [stream, setStream] = useState()
+	const [receivingCall, setReceivingCall] = useState(false)
+	const [caller, setCaller] = useState("")
+	const [callerSignal, setCallerSignal] = useState()
+	const [callAccepted, setCallAccepted] = useState(false)
+	const [idToCall, setIdToCall] = useState("")
+	const [callEnded, setCallEnded] = useState(false)
+	const [name, setName] = useState("")
+	const myVideo = useRef()
+	const userVideo = useRef()
+	const connectionRef = useRef()
+
+	useEffect(() => {
+		navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+			setStream(stream)
+			myVideo.current.srcObject = stream
+		})
+
+		socket.on("me", (id) => {
+			setMe(id)
+		})
+
+		socket.on("callUser", (data) => {
+			setReceivingCall(true)
+			setCaller(data.from)
+			setName(data.name)
+			setCallerSignal(data.signal)
+		})
+	}, [])
+
+	const callUser = (id) => {
+		const peer = new Peer({
+			initiator: true,
+			trickle: false,
+			stream: stream
+		})
+		peer.on("signal", (data) => {
+			socket.emit("callUser", {
+				userToCall: id,
+				signalData: data,
+				from: me,
+				name: name
+			})
+		})
+		peer.on("stream", (stream) => {
+
+			userVideo.current.srcObject = stream
+
+		})
+		socket.on("callAccepted", (signal) => {
+			setCallAccepted(true)
+			peer.signal(signal)
+		})
+
+		connectionRef.current = peer
+	}
+
+	const answerCall = () => {
+		setCallAccepted(true)
+		const peer = new Peer({
+			initiator: false,
+			trickle: false,
+			stream: stream
+		})
+		peer.on("signal", (data) => {
+			socket.emit("answerCall", { signal: data, to: caller })
+		})
+		peer.on("stream", (stream) => {
+			userVideo.current.srcObject = stream
+		})
+
+		peer.signal(callerSignal)
+		connectionRef.current = peer
+	}
+
+	const leaveCall = () => {
+		setCallEnded(true)
+		connectionRef.current.destroy()
+	}
+	console.log(idToCall)
+
+	return (
+		<>
+			<div>
+				<div>
+					{stream && <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+				</div>
+				<div className="video">
+					{callAccepted && !callEnded ?
+						<video playsInline ref={userVideo} autoPlay style={{ width: "300px" }} /> :
+						null}
+				</div>
+			</div>
+
+
+			<div>
+				<input
+					id="filled-basic"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<CopyToClipboard text={me}>
+					<button>
+						Copy ID
+					</button>
+				</CopyToClipboard>
+
+				<input
+					id="filled-basic"
+					value={idToCall}
+					onChange={(e) => setIdToCall(e.target.value)}
+				/>
+				<div>
+					{callAccepted && !callEnded ? (
+						<button onClick={leaveCall}>
+							End Call
+						</button>
+					) : (
+						<button onClick={() => callUser(idToCall)}>
+							call
+						</button>
+					)}
+					{idToCall}
+				</div>
+			</div>
+			<div>
+				{receivingCall && !callAccepted ? (
+					<div className="caller">
+						<h1 >{name} is calling...</h1>
+						<button onClick={answerCall}>
+							Answer
+						</button>
+					</div>
+				) : null}
+			</div>
+		</>
+	)
+}
+
+export default App
