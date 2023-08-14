4401f1b789a61bea2e1c04a343a38a59d5d0f96b
iqbal <111554475+iqbalcodes6602@users.noreply.github.com>
2023-08-14 18:17:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const http = require("http")
 const app = express()
 const server = http.createServer(app)
+
 const io = require("socket.io")(server, {
 	cors: {
 		origin: "http://localhost:3000",
